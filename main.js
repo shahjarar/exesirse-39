5a450b65f8156f43822ccec8d9ec0476c8eb5f53
shahjarar <160387866+shahjarar@users.noreply.github.com>
2024-02-29 18:25:28 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,13 @@
+// Task 39 
+// City names : write a functio:
+//"lahore, pakistan"
+// call your function with at least three city-country pairs , and print the value that's returned
+function cityCountry(city, country) {
+    return '$(city),${counrty}';
+}
+var c1 = cityCountry('lahore', 'pakistan');
+var c2 = cityCountry('tokyo', 'japan');
+var c3 = cityCountry('paris', 'france');
+console.log('lahore', 'pakistan');
+console.log('tokoyo', 'japan');
+console.log('paris', 'france');
---
+++ main.ts
@@ -0,0 +1,17 @@
+// Task 39 
+
+
+// City names : write a functio:
+//"lahore, pakistan"
+// call your function with at least three city-country pairs , and print the value that's returned
+
+function cityCountry(city:  string, country: string): string {
+    return'$(city),${counrty}'
+
+}
+let c1 = cityCountry('lahore', 'pakistan')
+let c2 = cityCountry('tokyo','japan')
+let c3 = cityCountry('paris','france')
+console.log('lahore','pakistan')
+console.log('tokoyo','japan')
+console.log('paris','france')
